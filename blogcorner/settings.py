2821f172d3c8274ca0be8657cd5e744d209cfd86
Joseph-Opene <84211429+Joseph-Opene@users.noreply.github.com>
2021-05-22 10:36:52 -0700

    Update settings.py

--- blogcorner/settings.py
+++ blogcorner/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'td9@w7=%qv^j5fcovtpkp2p7i5cmc#01ga)xkaglv-(9h*po9a'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
